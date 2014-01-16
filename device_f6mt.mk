9282e85c02317daf459eefca867fedc4d963a8d5
PlayfulGod <playfulgod@gmail.com>
2014-01-16 00:22:48 -0500

    Add fstab.qcom

--- device_f6mt.mk
+++ device_f6mt.mk
@@ -14,6 +14,12 @@ else
 	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
 endif
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_KERNEL):kernel
+
+PRODUCT_COPY_FILES += \
+        $(LOCAL_PATH)/fstab.qcom:root/fstab.qcom
+    
 $(call inherit-product, build/target/product/full.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
